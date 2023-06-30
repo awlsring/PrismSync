140d601190d5eb5b240c05b7c2a265cd3465a005
Matthew Rawlings <mattcanemail@gmail.com>
2023-06-30 13:54:56 -0700

    Initial Commit

---
+++ PrismSync/PrismSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismSyncApp.swift
+//  PrismSync
+//
+//  Created by Matthew Rawlings on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PrismSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
